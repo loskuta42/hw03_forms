69fa4b76d662b4212c639ae2ca14542f3af13295
Alexey <loskuta42@yandex.ru>
2021-03-08 12:19:25 +0300

    add decorator login_required to new_post view function

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 from .models import Post, Group, User
 from .forms import PostForm
 
@@ -14,6 +15,7 @@ def group_posts(request, slug):
     return render(request, "group.html", {"group": group, "posts": posts})
 
 
+@login_required
 def new_post(request):
     if request.method != "POST":
         form = PostForm(request.POST)
