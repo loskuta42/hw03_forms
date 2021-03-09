8782f4ecbf7507f5438a1de79382f3bfdc4ae9a1
Alexey <loskuta42@yandex.ru>
2021-03-09 17:13:38 +0300

    add setUpClass in test_urls

--- posts/tests/test_urls.py
+++ posts/tests/test_urls.py
@@ -2,13 +2,15 @@ from django.test import TestCase, Client
 
 
 class StaticURLTests(TestCase):
-    def setUp(self):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
         # Устанавливаем данные для тестирования
         # Создаём экземпляр клиента. Он неавторизован.
-        self.guest_client = Client()
+        cls.guest_client = Client()
 
     def test_homepage(self):
         # Отправляем запрос через client,
         # созданный в setUp()
-        response = self.guest_client.get('/')
+        response = StaticURLTests.guest_client.get('/')
         self.assertEqual(response.status_code, 200)
