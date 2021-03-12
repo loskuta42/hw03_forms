9f9afc567d98ee28faeaf134950866960fa6ccb3
Alexey <loskuta42@yandex.ru>
2021-03-12 09:19:10 +0300

    add not working test_forms

---
+++ posts/tests/test_forms.py
@@ -0,0 +1,46 @@
+from django.test import Client, TestCase
+from django.urls import reverse
+from posts.forms import PostForm
+from django.contrib.auth import get_user_model
+from posts.models import Group, Post
+from django import forms
+
+User = get_user_model()
+
+
+
+class PostFormTests(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.guest_client = Client()
+        cls.user = User.objects.create_user(username='testuser')
+        cls.authorized_client = Client()
+        cls.authorized_client.force_login(cls.user)
+        cls.group = Group.objects.create(
+            title='test_group',
+            slug='test-slug',
+            description='test_description'
+        )
+
+    def test_create_post(self):
+        posts_count = Post.objects.count()
+        choice_field = forms.ChoiceField(widget=forms.Select).widget.choices()
+        form_data = {
+            'text': 'test_post',
+            'group': choice_field,
+        }
+        response = self.authorized_client.post(
+            reverse('new_post'),
+            data=form_data,
+            follow=True
+        )
+        self.assertRedirects(response, reverse('index'))
+        self.assertEqual(Post.objects.count(), posts_count+1)
+        self.assertTrue(
+            Post.objects.filter(
+                group=PostFormTests.group,
+                text='test_post',
+#                author=User.objects.get(username='test_user')
+                ).exists()
+        )
